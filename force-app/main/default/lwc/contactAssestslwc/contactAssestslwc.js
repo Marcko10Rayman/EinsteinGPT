411012332853302e9a70a04d3171d42fed2a099c
Marco Cruz <mcruz@pkglobal.com>
2024-04-26 10:53:49 -0600

    chore(lwc): finish lwc with the correct specifications

--- force-app/main/default/lwc/contactAssestslwc/contactAssestslwc.js
+++ force-app/main/default/lwc/contactAssestslwc/contactAssestslwc.js
@@ -1,3 +1,38 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track, api } from 'lwc';
+import contactAssets from '@salesforce/apex/ContactAssets_cls.getAssets';
 
-export default class ContactAssestslwc extends LightningElement {}
+export default class ContactAssestslwc extends LightningElement {
+
+    @api recordId;
+    @track assets;
+    @track error;
+    @track isData = false;
+
+    connectedCallback() {
+        this.getAssets();
+    }
+
+    getAssets() {
+        let idCall = this.recordId;
+        console.info('Id ::: ' + idCall);
+        contactAssets({idVoiceCall:idCall})
+        .then(result => {
+            this.assets = result;
+            console.log('Tamaño del arreglo');
+            console.log(result.length);
+            console.dir(result);
+            if(result.length > 0) {
+                this.isData = true;
+                console.log('change boolean');
+            }
+            console.log(this.assets);
+        })
+        .catch(error => {
+            this.error = error;
+        })
+    }
+
+
+
+
+}
